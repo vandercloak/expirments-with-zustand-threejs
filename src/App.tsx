204f1c9206c0daf488b1ea5a037f30712516550f
Nick Vander Kolk <nicholas.vanderkolk@gmail.com>
2022-04-22 17:58:02 -0500

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,82 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { createImmerStore } from "./create-immer-store";
+import Table from "react-bootstrap/Table";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+type Bear = {
+  name: string;
+  age: number;
+  color: "brown" | "black" | "white";
+  hibernating: "💤" | "👀";
+};
+
+type Cave = {
+  bears: Array<Bear>;
+  getBearIdx: (name: string) => number;
+  addBears: (bear: Bear) => void;
+  removeBear: (idx: number) => void;
+};
+
+const useStore = createImmerStore<Cave>((set, get, api) => ({
+  bears: [],
+  getBearIdx: (name) => {
+    return get().bears.findIndex((b) => b.name === name);
+  },
+  addBears: (bear: Bear) => {
+    return set((state) => {
+      state.bears.push(bear);
+    });
+  },
+  removeBear: (idx: number) => {
+    return set((state) => {
+      if (idx === -1) {
+        return;
+      }
+      state.bears.splice(idx, 1);
+    });
+  },
+}));
+
+const bears: Array<Bear> = [
+  {
+    name: "Gilbert",
+    age: 12,
+    color: "brown",
+    hibernating: "👀",
+  },
+];
 
 function App() {
+  const caveStore = useStore();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        🐻{" "}
+        {/* {bears.map((bear) => (
+          <div>{bear.name}</div>
+        ))} */}
+      </div>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Name</th>
+            <th>Age</th>
+            <th>Color</th>
+            <th>Hibernating</th>
+          </tr>
+        </thead>
+        <tbody>
+          {bears.map((bear, index) => (
+            <tr>
+              <td>{index + 1}</td>
+              <td>{bear.name}</td>
+              <td>{bear.age}</td>
+              <td>{bear.color}</td>
+              <td>{bear.hibernating}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
     </div>
   );
 }
---
+++ src/create-immer-store.ts
@@ -0,0 +1,9 @@
+import create, { State, StateCreator, StoreMutatorIdentifier } from "zustand";
+import { immer } from "zustand/middleware/immer";
+
+export function createImmerStore<
+  T extends State,
+  Mos extends [StoreMutatorIdentifier, unknown][] = []
+>(initializer: StateCreator<T, [...Mos, ["zustand/immer", never]]>) {
+  return create<T, Mos>(immer<T, Mos>(initializer) as any);
+}
